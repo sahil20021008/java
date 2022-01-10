3b69716f77205ffb8ffadd221bba087754b05f99
Sahil Goyal <87982509+sahil20021008@users.noreply.github.com>
2022-01-10 18:07:04 +0530

    Update strong_password.java

--- strong password suggestion/strong_password.java
+++ strong password suggestion/strong_password.java
@@ -10,26 +10,20 @@ public class strong_password {
     static String values = capital_chars + small_chars + numbers + symbols;
 
     public static void strong_password_generator() {
-    	String s="";
+    	ArrayList<String>se=new ArrayList<>();
     	Random random=new Random();
     	int n=random.nextInt(12) + 8;
-    	s+=String.valueOf(capital_chars.charAt(random.nextInt(capital_chars.length())));
-    	s+=String.valueOf(small_chars.charAt(random.nextInt(small_chars.length())));
-    	s+=String.valueOf(numbers.charAt(random.nextInt(numbers.length())));
-    	s+=String.valueOf(symbols.charAt(random.nextInt(symbols.length())));
+    	se.add(String.valueOf(capital_chars.charAt(random.nextInt(capital_chars.length()))));
+    	se.add(String.valueOf(small_chars.charAt(random.nextInt(small_chars.length()))));
+    	se.add(String.valueOf(numbers.charAt(random.nextInt(numbers.length()))));
+    	se.add(String.valueOf(symbols.charAt(random.nextInt(symbols.length()))));
     	for(int i=0;i<n-4;i++) {
-    		s+=String.valueOf(values.charAt(random.nextInt(values.length())));
-    	}
-    	ArrayList<String>se=new ArrayList<>();
-    	for(int i=0;i<n;i++) {
-    		se.add(s.substring(i, i+1));
+    		se.add(String.valueOf(values.charAt(random.nextInt(values.length()))));
     	}
     	Collections.shuffle(se);
-    	s="";
     	for(int i=0;i<n;i++) {
-    		s+=se.get(i);
+    		System.out.print(se.get(i));
     	}
-    	System.out.println(s);
     }
     
 	public static void main(String[] args) {
